204f50dfb98b124245924f7fe28ac6ab1e819201
Pedro Fonseca <pedrojgfonseca@gmail.com>
2023-11-11 19:33:56 +0000

    Created a terminal for managing DBs. Made Hashing Ring work with the new CRUD of the DBs.

--- Server/Server_ZMQ.py
+++ Server/Server_ZMQ.py
@@ -1,9 +1,12 @@
+import threading
+import requests
 import zmq
 import os
 import sys
 import json
 import hashlib
 import time
+from prettytable import PrettyTable
 
 from jwt import InvalidSignatureError
 
@@ -195,7 +198,8 @@ class Server:
         socket = context.socket(zmq.REP)
         socket.connect("tcp://localhost:5560")
 
-        while True:
+        global tm # checks if terminal manager is running
+        while tm.is_alive():
             request = socket.recv_json()
             response = None
             try:
@@ -211,7 +215,136 @@ class Server:
             response = self.request_handlers[request['type']](self.remove_attributes(request))
             socket.send_json(response)
 
+class RequestsServer(threading.Thread):
+    def __init__(self):
+        super(RequestsServer, self).__init__(name="Requests Server Thread")
+        self._stop_event = threading.Event()
+
+    def run(self):
+        global server
+        server.run()
+
+    def stop(self):
+        self._stop_event.set()
+
+class DBTerminalManagement(threading.Thread):
+    def __init__(self):
+        super(DBTerminalManagement, self).__init__(name="DB Terminal Management Thread")
+        print('DB Terminal Management Online')
+
+    def print_table(self, data_dict):
+        table = PrettyTable()
+        table.field_names = list(data_dict.keys())
+
+        for i in range(len(data_dict[list(data_dict.keys())[0]])):
+            row = [data_dict[key][i] for key in data_dict.keys()]
+            table.add_row(row)
+
+        print(table)
+
+    def run(self):
+        global server
+        print("Welcome to the DB Management Terminal!")
+        print("Write 'help' to get started")
+        command = ""
+        args = []
+        while command.strip().lower() != "quit":
+            received = input("> ")
+            command = received.split(' ')[0]
+            args = received.split(' ')[1:]
+
+            if command == "help":
+                print("Available Commands:")
+                print("\n\thelp - Shows a list of the existing commands with a comprehensive explanation.\n")
+                print("\tlist - Lists the current existing DBs. Shows their ids, current state (ONLINE, OFFLINE), number of requests and number of shopping lists.\n")
+                print("\tcreate - Creates a new DB for the server. The id of this DB is automatically assigned.\n")
+                print("\tenable [id]- Enables a currently disabled DB. Receives an argument id which represents the id of the DB.\n")
+                print("\tdisable [id] - Disables an existing DB from the server. Receives an argument id which represents the id of the DB.\n")
+                print("\tquit - Turns off the server and this management terminal.")
+                print("\nNotes:")
+                print("\n\tArguments can always be passed in front of any command. The only arguments that will ever be used are for commands that ask for them. Any command will alert in the case of a nonexistent/invalid argument.")
+            elif command == "create":
+                new_db_id = server.db_management.create_database()
+                server.hashing_ring = HashingRing(len(server.db_management.database_connections.keys()))
+                print(f"Successfully created a new DB with the ID {new_db_id}")
+            elif command in ["enable", "disable"]:
+                if len(args) == 0:
+                    print("You didn't provide any id")
+                    continue
+                try:
+                    provided_id = int(args[0])
+                except ValueError:
+                    print("The id provided is not a valid number")
+                    continue
+
+                if provided_id not in server.db_management.database_connections_state:
+                    print("The id you provided doesn't match any existent database id")
+                    continue
+
+                if command == "enable":
+                    if server.db_management.database_connections_state[provided_id]:
+                        print(f"The database with the ID {provided_id} was already enabled.")
+                    else:
+                        server.db_management.database_connections_state[provided_id] = True
+                        print(f"Successfully enabled database with the ID {provided_id}")
+                else:
+                    if not server.db_management.database_connections_state[provided_id]:
+                        print(f"The database with the ID {provided_id} was already disabled.")
+                    else:
+                        server.db_management.database_connections_state[provided_id] = False
+                        print(f"Successfully disabled database with the ID {provided_id}.")
+            elif command == "list":
+                db_ids = list(server.db_management.database_connections.keys())
+                db_states = ["ONLINE" if server.db_management.database_connections_state[key] else "OFFLINE" for key in db_ids]
+                db_num_requests = [server.db_management.database_connections_num_requests[key] for key in db_ids]
+                db_num_lists = [server.db_management.database_connections_num_lists[key] for key in db_ids]
+                table_dict = {"Database ID": db_ids, "State": db_states, "Nº Requests": db_num_requests, "Nº Lists": db_num_lists}
+                self.print_table(table_dict)
+            elif command == "quit":
+                pass
+            else:
+
+                suggestions = ["help", "create", "enable", "disable", "list"]
+                min_correspondence = 3  # Minimum correspondence to consider a suggestion
+
+                suggestion = ""
+                max_matches = 0
+
+                for possible in suggestions:
+                    matches = sum(a == b for a, b in zip(command, possible))
+
+                    if matches > max_matches and matches >= min_correspondence:
+                        suggestion = possible
+                        max_matches = matches
+
+                if suggestion != "":
+                    print(f"The command '{command}' does not exist. Did you mean '{suggestion}'?")
+                else:
+                    print(f"The command '{command}' does not exist. Write 'help' for possible commands.")
+
+
+        print("Thank you for using our system! Quitting.")
+
 
 if __name__ == '__main__':
     server = Server()
-    server.run()
+
+    rs = RequestsServer()
+    tm = DBTerminalManagement()
+
+    print('Requests Server Online')
+    rs.start()
+
+    tm.start()
+
+    while tm.is_alive():
+        if not rs.is_alive():
+            # if RequestsServer crashes - we start it up again
+            rs = RequestsServer()
+            rs.start()
+
+    ## send a random request for RequestsServer to verify that tm is no longer running
+    while rs.is_alive():
+        pass
+
+    print("Shutting Down...")
