954f1d4d59304cb4f72160aaf2fa6d4c8b6e9734
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2023-11-24 11:47:53 +0000

    Added loader (not completely finished)

--- Client/static/js/main.js
+++ Client/static/js/main.js
@@ -1,3 +1,19 @@
 particlesJS.load('particles-js', '../static/config/particles.json', function() {
     console.log('particles.js loaded - callback');
   });
+
+window.addEventListener("DOMContentLoaded", function () {
+  const loader = document.querySelector(".loader");
+  const overlay = this.document.querySelector(".overlay");
+  overlay.style.opacity = 0;
+  overlay.style.visibility = 'hidden';
+  loader.style.opacity = 0;
+  loader.style.visibility = 'hidden';
+
+  window.addEventListener("beforeunload", function () {
+    overlay.style.opacity = 1;
+    overlay.style.visibility = 'visible';
+    loader.style.opacity = 1;
+    loader.style.visibility = 'visible';
+  });
+});
